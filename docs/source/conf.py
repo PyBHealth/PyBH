148302cb99ed1687b6812a0e075050a4dd7c659c
Jean Van Dyk <jeanvandyk14@gmail.com>
2025-10-15 21:43:16 +0200

    Automatic documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,9 +3,15 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../.."))
+
 # autodoc_mock_imports
 # This avoids autodoc breaking when it can't find packages imported in the code.
 # https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#confval-autodoc_mock_imports # noqa: E501
+
 autodoc_mock_imports = [
     "arviz",
     "matplotlib",
